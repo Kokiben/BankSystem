dab0aa71650a20835b2e052b9c29f7837e825a58
Kokiben <bennassarkaoutar@gmail.com>
2025-04-16 03:43:23 +0100

    Add project files.

---
+++ BankSystem/BankSystem.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "clsLoginScreen.h"
+
+int main()
+{
+    while (true)
+    {
+        clsLoginScreen::ShowLoginScreen();
+
+    }
+    system("pasue>0");
+    return 0;
+}
+
